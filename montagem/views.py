f1bd913ffaa4766abad3425bc9433730e89591f2
Richard Machado <richardmachado.93@gmail.com>
2019-11-10 22:04:48 -0300

    incluido a paginação

--- montagem/views.py
+++ montagem/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render,redirect,get_object_or_404
+from django.core import paginator
+from django.core.paginator import Paginator,PageNotAnInteger,EmptyPage
 from django.http.response import HttpResponse
 from django.core import serializers
 from .models import Person
@@ -10,22 +12,35 @@ def home(request):
 def listaServos(request):
     serial = False    
     query = request.GET.get("busca",'')
-    servos = consultaBanco(serial,query)
-    sizeList = len(servos)
+    sort = request.GET.get("ordenar", '')
+    page = request.GET.get("page", '')
+    params = [serial,query,sort]
 
-    if request.method == 'GET':
         
+    try:
+        servos = consultaBanco(params)
+        servos = Paginator(servos,25)
+        servos = servos.page(page)
+    except  PageNotAnInteger :
+        servos = servos.page(1)
+    except EmptyPage :
+        servos = paginator.page(paginator.num_page)
+
+
+    if request.method == 'GET':        
         return render(request,'lista_servos.html',{'servos':servos})
 
     elif request.method == 'POST':
         return HttpResponse ("Logo faz algo")
     
 
-def consultaBanco(serial,query):
-    if serial == True:
+def consultaBanco(params):
+    if params[0] == True:
          data = serializers.serialize("json", Person.objects.all())
-    elif query:
-         data = Person.objects.filter(nome__icontains = query)
+    elif params[1]:
+         data = Person.objects.filter(nome__icontains = params[1])
+    elif params[2]:
+         data = Person.objects.all().order_by(params[2])     
     else:
          data = Person.objects.all()
     return data
